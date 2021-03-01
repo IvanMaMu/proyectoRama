6510d94adba5c2a1842a4ff9b39f9ae7de82b434
CEINMARK <CEINMARK@DESKTOP-4GR91FV>
2021-03-01 09:06:34 +0100

    rama

---
+++ src/prueba/rama1.java
@@ -0,0 +1,15 @@
+package prueba;
+
+import java.util.Scanner;
+
+public class rama1 {
+	
+	public static void main(String arg[]){
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("¿cuantos años tienes?");
+		int años=sc.nextInt();
+	}
+
+}
